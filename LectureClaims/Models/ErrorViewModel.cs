67de484c11b3c4041c01c9aeda3cdec13f669b35
st10294145@vcconnect.edu.za <ST10294145@vcconnect.edu.za>
2024-11-20 14:35:22 +0200

    Add project files.

---
+++ LectureClaims/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LectureClaims.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
