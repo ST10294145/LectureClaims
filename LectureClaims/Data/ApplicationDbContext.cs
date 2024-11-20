3434a789beb2a9b965ecece4f5d0749d0b80183c
st10294145@vcconnect.edu.za <ST10294145@vcconnect.edu.za>
2024-11-20 16:29:30 +0200

    Installed Identity FrameWork core package This will help when scaffolding identities for registration and login.

--- LectureClaims/Data/ApplicationDbContext.cs
+++ LectureClaims/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using LectureClaims.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace LectureClaims.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
