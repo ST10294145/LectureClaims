9bd8d39e2c182b41a3537f53e0fb0c35b9dc5ef9
st10294145@vcconnect.edu.za <ST10294145@vcconnect.edu.za>
2024-11-20 15:13:08 +0200

    Added a data folder This is used to inject the table of users inot the database.

---
+++ LectureClaims/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using LectureClaims.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LectureClaims.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<ApplicationUser> Users { get; set; }
+    }
+}
